2a13e9b4baa3424bf367552bd187357d10a72511
MPY <877455134@qq.com>
2017-06-27 15:23:46 +0800

    no message

---
+++ MMDYZB/MMDYZB/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MMDYZB
+//
+//  Created by jqugo on 2017/6/27.
+//  Copyright © 2017年 jqugo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+
